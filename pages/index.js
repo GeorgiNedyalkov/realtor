351426c2568b5d84ab93a80613e4bf7f9002a5fa
Flu3nt <gnedyalkov94@gmail.com>
2022-11-29 18:10:59 +0200

    Create the home and search pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
-
 import { fetchApi, baseUrl } from "../utils/fetchApi";
 import Property from "../components/Property";
 
